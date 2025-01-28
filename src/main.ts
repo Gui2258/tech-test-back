662eb4d24ce76dd9a7e959e8bf6c10499cf24132
Guido Morffi <guidotele@gmail.com>
2025-01-28 12:14:09 -0500

    Added typeorm an conect with database

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'http://localhost:3000',
+        origin: 'http://localhost:3001',
     });
 
     app.useGlobalPipes(
