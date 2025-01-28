ad1db39701eb6d8b76e061ac901f1de93942e366
Guido Morffi <guidotele@gmail.com>
2025-01-28 11:45:30 -0500

    Added swagger an configure global pipes for validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: 'http://localhost:3000',
+    });
+
+    app.useGlobalPipes(
+        new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })
+    );
+
+    app.setGlobalPrefix('api');
+    await app.listen(process.env.PORT ?? 3000);
+
+    const config = new DocumentBuilder()
+        .setTitle('Cuban Enginier Tech Test')
+        .setDescription('Backend of the Cuben Enginier Tech Test')
+        .setVersion('0.1')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/swagger', app, document);
 }
 bootstrap();
