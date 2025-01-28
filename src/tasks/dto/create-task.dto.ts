38e67a37aa746fd2b6b8cb333f0b733bd6c9f257
Guido Morffi <guidotele@gmail.com>
2025-01-28 13:04:20 -0500

    Configured task module

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,10 @@
-export class CreateTaskDto {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MinLength } from 'class-validator';
+
+export class CreateTaskDto {
+    @ApiProperty({ description: 'The title of the product' })
+    @IsString()
+    @MinLength(3)
+    content: string;
+}
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,11 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Column, PrimaryGeneratedColumn } from 'typeorm';
 export class Task {
-    @ApiProperty({ description: 'The unique identifier of the product' })
+    @ApiProperty({ description: 'The unique identifier of the task' })
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @ApiProperty({ description: 'The title of the product' })
+    @ApiProperty({ description: 'Task content' })
     @Column('text')
     content: string;
+
+    @ApiProperty({ description: 'Task status' })
+    @Column('bool', { default: false })
+    checkDone: boolean;
 }
