20c220c9c5a0d1408df83aa883951a3904109759
Guido Morffi <guidotele@gmail.com>
2025-01-28 12:49:32 -0500

    Added task entyti

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,11 @@
-export class Task {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+export class Task {
+    @ApiProperty({ description: 'The unique identifier of the product' })
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @ApiProperty({ description: 'The title of the product' })
+    @Column('text')
+    content: string;
+}
